e8484c967102a4b000ffd823caa042104ccb8599
Mitzi17 <m_estrada17@live.com>
2022-06-15 23:18:00 -0700

    commented out React.StrictMode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-ReactDOM.createRoot(document.getElementById('root'));
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>
-//);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ //<React.StrictMode>
+   <App />
+ //</React.StrictMode>
+);
 
