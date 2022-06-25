19dc81c2b23f992df4e7d5c24bacba33ad5fbb3a
Mitzi17 <m_estrada17@live.com>
2022-06-25 12:45:51 -0700

    import packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import  { createStore, applyMiddleware } from 'redux'
+import thunk from 'react-thunk'
+import { Provider } from 'react-redux'
+'
 import App from './App';
 
 
