a6a29b40b0f2936005c86f7af7e1f3d289f10d63
Mitzi17 <m_estrada17@live.com>
2022-06-27 23:20:17 -0700

    create file and use dispatch inside thunk function to create an account

---
+++ src/actions/addAccount.js
@@ -0,0 +1,17 @@
+export const addAccount = (data) => {
+
+return (dispatch) => {
+    fetch('http://localhost:3000/accounts', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(account => dispatch({type: 'ADD_ACCOUNT', payload: account}))
+
+}
+
+}
