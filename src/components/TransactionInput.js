935247644964cb2f9fd370bb7915de1349486cba
Mitzi17 <m_estrada17@live.com>
2022-08-12 17:29:19 -0700

    create file for Transaction form

---
+++ src/components/TransactionInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class TransactionInput extends React.Component {
+
+    render() {
+        return(
+
+            <div>
+                TransactionInput
+            </div>
+
+        )
+    }
+
+}
+
+export default TransactionInput
