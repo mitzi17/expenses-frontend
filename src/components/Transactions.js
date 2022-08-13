03e49c0badc15f24c37c988d5378de5d3c45ad19
Mitzi17 <m_estrada17@live.com>
2022-08-12 22:41:13 -0700

    map over an accounts transactions to display them

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -5,6 +5,8 @@ const Transactions = (props) => {
     return (
         <div>
             Transactions
+            {props.transactions && props.transactions.map(transaction => 
+                <li key={transaction.id}> {transaction.kind} - {transaction.amount} - {transaction.date} </li>)}
         </div>
     )
 
