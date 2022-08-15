c85aee731c7b8ad68e0f223c93d1d2a452a11678
Mitzi17 <m_estrada17@live.com>
2022-08-14 22:32:28 -0700

    define handleChange and handleSubmit

--- src/components/TransactionInput.js
+++ src/components/TransactionInput.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { addTransaction } from '../actions/addTransaction'
 
 class TransactionInput extends React.Component {
+
+    state = {
+        kind: 'deposit',
+        amount: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addTransaction(this.state, this.props.account.id)
+        this.setState({
+            kind: 'deposit',
+            amount: ''
+        })
+    }
 
     render() {
         return(
 
             <div>
                 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Transaction Type:</label>
-                    <select>
+                    <select name='kind' value={this.state.kind} onChange={this.handleChange}>
                         <option>deposit</option>
                         <option>withdraw</option>
                     </select>
                     <label>Transaction Amount:</label>
-                    <input type="text"/>
+                    <input type="text" name='amount' value={this.state.amount} onChange={this.handleChange}/>
+                    <input type="submit"/>
                 </form>
             </div>
 
@@ -24,4 +46,4 @@ class TransactionInput extends React.Component {
 
 }
 
-export default connect(null)(TransactionInput)
+export default connect(null, {addTransaction})(TransactionInput)
