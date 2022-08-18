b328ab835060bbabcb918e9bc9bb7c54a23e35a0
Mitzi17 <m_estrada17@live.com>
2022-08-17 17:43:52 -0700

    connected component and define handleDelete

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteTransaction } from '../actions/deleteTransaction'
 
 const Transactions = (props) => {
+
+const handleDelete = (transaction) => {
+    props.deleteTransaction(transaction.id, transaction.account_id)
+
+
+}
 
     return (
         <div>
             Transactions
             {props.transactions && props.transactions.map(transaction => 
-                <li key={transaction.id}> {transaction.kind} - {transaction.amount} - {transaction.date} </li>)}
+                <li key={transaction.id}> {transaction.kind} - {transaction.amount} - {transaction.date} <button onClick={() => handleDelete(transaction)}>DELETE</button></li>)}
         </div>
     )
 
 }
 
-export default Transactions
+export default connect(null, {deleteTransaction}) (Transactions)
