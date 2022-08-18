756d013c75f5f3127263a49705f12e4ca389ab74
Mitzi17 <m_estrada17@live.com>
2022-08-17 17:45:12 -0700

    create file and define deleteTransaction action creator

---
+++ src/actions/deleteTransaction.js
@@ -0,0 +1,14 @@
+export const deleteTransaction = (transactionId, accountId) => {
+
+    return (dispatch) => {
+        return fetch(`http://localhost:3000/accounts/${accountId}/transactions/${transactionId}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(account => dispatch({type: 'DELETE_TRANSACTION', payload: account}))
+        
+
+
+    }
+
+}
