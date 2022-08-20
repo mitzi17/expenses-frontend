1c96a2a1975611ec51a85e8ff3f97ad6f8357333
Mitzi17 <m_estrada17@live.com>
2022-08-19 17:39:24 -0700

    add case for editing account

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -3,8 +3,20 @@ export default function accountReducer(state = {accounts: []}, action) {
     switch (action.type) {
         case 'FETCH_ACCOUNTS':
             return {accounts: action.payload}
+
         case 'ADD_ACCOUNT':
             return {...state, accounts: [...state.accounts, action.payload]}
+
+        case 'EDIT_ACCOUNT':
+            let accountsArrayForEdit = state.accounts.map(account => {
+                if (account.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return account
+                }
+            })
+            return {...state, accounts: accountsArrayForEdit}    
+            
         case 'ADD_TRANSACTION':
             let accounts = state.accounts.map(account => {
                 if (account.id === action.payload.id) {
@@ -14,6 +26,7 @@ export default function accountReducer(state = {accounts: []}, action) {
                 }
             })
             return {...state, accounts: accounts}
+            
         case 'DELETE_TRANSACTION':
             let accountsArray = state.accounts.map(account => {
                 if (account.id === action.payload.id) {
