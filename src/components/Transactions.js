4b582e31aade021b647bcf6e01ca404dd83b107d
Mitzi17 <m_estrada17@live.com>
2022-08-26 23:23:15 -0700

    add p tag for subsections of transaction

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -13,8 +13,9 @@ const handleDelete = (transaction) => {
     return (
         <div class="box">
             <p class="title is-4 has-text-link has-text-left">All Transactions</p>
+            <p class="title is-6 has-text-link has-text-left">   -  Type - Amount - Date - </p>
             {props.transactions && props.transactions.map(transaction => 
-                <div class="box" key={transaction.id}> {transaction.kind} - {transaction.amount} - {transaction.date} <button onClick={() => handleDelete(transaction)}>DELETE</button></div>)}
+                <div class="box" key={transaction.id}> {transaction.kind} - ${transaction.amount} - {transaction.date} <button onClick={() => handleDelete(transaction)}>DELETE</button></div>)}
         </div>
     )
 
