42fe38623bd42f77c0ca08bbeddc1d87086fc62e
Mitzi17 <m_estrada17@live.com>
2022-10-05 16:49:56 -0700

    connected deleteAccount from actions

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-import Account from './Account'
-
+import { connect } from 'react-redux'
+import { deleteAccount } from '../actions/deleteAccount'
 
 const Accounts = (props) => {
+
+    const handleDelete = (account) => {
+        props.deleteAccount(account.id)
+    }
 
     return (
 
@@ -12,11 +16,10 @@ const Accounts = (props) => {
             {props.accounts.map(account =>
                 <div class="box" key={account.id}>
                     <Link to={`/accounts/${account.id}`}>{account.name}</Link>
+                <button class="button is-danger is-light is-small is-rounded has-addons is-right" onClick={() => handleDelete(account)}>DELETE</button>
                 </div>)}
         </div>
-
     )
-
 }
 
-export default Accounts
+export default connect(null, {deleteAccount}) (Accounts)
